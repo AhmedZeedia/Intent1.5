974d03b9aecd38381c4d9cec72e35d36e50aaf5f
azeedia <azeedia@starmicronics.com>
2020-01-02 14:07:15 -0600

    commeit

--- app/src/main/java/com/example/intentforall/SecondActivity.java
+++ app/src/main/java/com/example/intentforall/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.example.intentforall;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
     }
 }
